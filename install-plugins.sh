8a5a4d0e13e4eb82d5187b5304d64c67b311994e
David Bogner <davidbogner@gmail.com>
2021-08-02 02:08:07 +0200

    Update install-plugins.sh

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 moodleroot="/var/www/moodle"
-moodleversion="MOODLE_11_STABLE"
+moodleversion="MOODLE_311_STABLE"
 # Latest released Moodle version
-latestmoodle="MOODLE_11_STABLE"
+latestmoodle="MOODLE_311_STABLE"
 
+# Install theme moove
 cd $moodleroot/theme/
 git clone https://github.com/willianmano/moodle-theme_moove.git moove
 cd moove
@@ -14,4 +15,31 @@ then
 else
   echo "Check out $moodleversion"
   git checkout $moodleversion
+fi
+
+# Install mod_checklist
+cd $moodleroot/mod/
+git clone https://github.com/davosmith/moodle-checklist.git checklist
+
+# Install Level up
+cd $moodleroot/blocks/
+git clone https://github.com/FMCorz/moodle-block_xp.git xp
+
+# Install customcert
+cd $moodleroot/mod/
+git clone https://github.com/mdjnelson/moodle-mod_customcert.git customcert
+cd customcert
+git checkout moodleversion
+
+# Install course format tiles
+cd $moodleroot/course/format
+git clone https://bitbucket.org/dw8/moodle-format_tiles.git tiles
+cd tiles
+if [ "$moodleversion" = "MOODLE_311_STABLE" ]
+then
+  echo "Check out moodle311"
+  git checkout moodle311
+else
+  echo "Check out master"
+  git checkout master
 fi
